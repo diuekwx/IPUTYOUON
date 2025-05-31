92fee8c348def8eed2483dd0407cc2fc93974c2f
victoriav22 <victoria.vo256@gmail.com>
2025-05-30 19:14:18 -0700

    user playlist page is responsive

--- src/Components/HomeButton.tsx
+++ src/Components/HomeButton.tsx
@@ -1,19 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import home from '../assets/home.png';
+import './HomeButton.css';
 
 export default function HomeButton() {
     const navigate = useNavigate();
 
     return (
-        <button style={{
-            background: 'none',
-            border: 'none',
-            position: 'absolute',
-            width: '50px',
-            height: '50px',
-            top: '15px',
-            left: '15px',
-            padding: 0
-        }} onClick={() => navigate('/')}><img src={home}></img></button>
+        <button className="home-button" onClick={() => navigate('/')}><img src={home}></img></button>
     )
 }
